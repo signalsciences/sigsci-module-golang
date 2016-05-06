66d7ee18902b0111321107f6f22a2dea591436b2
Nick Galbreath <nickg@signalsciences.com>
2016-05-05 18:50:47 -0700

    cleanup, renames

--- module_test.go
+++ module_test.go
@@ -55,3 +55,12 @@ func TestNewRPCMsgFromRequest(t *testing.T) {
 		t.Errorf("NewWafMsgFromRequest: incorrect %q", ne)
 	}
 }
+
+
+// helper functions
+func TestStripPort(t *testing.T) {
+	got := StripPort("127.0.0.1:8000")
+	if got != "127.0.0.1" {
+		t.Errorf("StripPort(%q) = %q, want %q", "127.0.0.1:8000", got, "127.0.0.1")
+	}
+}
