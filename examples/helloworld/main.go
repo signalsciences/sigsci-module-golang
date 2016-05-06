2faa36e342c4f73b4bd1eb8ade2851cbf516b1d7
Nick Galbreath <nickg@signalsciences.com>
2016-05-05 23:29:09 -0700

    fix weird github checking

--- examples/helloworld/main.go
+++ examples/helloworld/main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
-	"net/http/httputil"
+	"time"
 
 	sigsci "github.com/signalsciences/sigsci-module-golang"
 )
 
 func helloworld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", "world")
+	fmt.Fprintf(w, "Hello, %q\n", "world")
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/helloworld", helloworld)
 
+	h, err := sigsci.NewModule(mux, sigsci.Timeout(50 * time.Millisecond))
+	if err != nil {
+		log.Fatal(err)
+	}
 	s := &http.Server{
-		Handler:        sigsci.NewAgentHandler(mux),
-		Addr:           serverAddr,
+		Handler: h,
+		Addr:    "127.0.0.1:9999",
 	}
 	log.Fatal(s.ListenAndServe())
 }
