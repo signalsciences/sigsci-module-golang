90afef0972348642d5c28c19ac6bf0324818b929
Nick Galbreath <nickg@signalsciences.com>
2016-09-02 23:23:35 +0000

    builder for jenkins

---
+++ make-jenkins.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if [ -z "${BUILD_NUMBER}" ]; then
+	echo "Must be run in Jenkins with BUILD_NUMBER set"
+	exit 2
+fi
+
+set -ex
+
+BASE=$PWD
+## setup our package properties by distro
+PKG_NAME="sigsci-module-golang"
+DST_BUCKET="s3://package-build-artifacts/${PKG_NAME}/${BUILD_NUMBER}"
+VERSION=$(cat ./VERSION)
+
+make
+make release
+
+cd ${BASE}
+aws s3 cp \
+        --no-follow-symlinks \
+        --cache-control="max-age=300" \
+        ./artifacts/${PKG_NAME}.tar.gz ${DST_BUCKET}/${PKG_NAME}_${VERSION}.tar.gz
+
+aws s3 cp \
+        --no-follow-symlinks \
+        --cache-control="max-age=300" \
+        --content-type="text/plain; charset=UTF-8" \
+        VERSION ${DST_BUCKET}/VERSION
+
+aws s3 cp \
+        --no-follow-symlinks \
+        --cache-control="max-age=300" \
+        --content-language="en-US" \
+        --content-type="text/markdown; charset=UTF-8" \
+        CHANGELOG.md ${DST_BUCKET}/CHANGELOG.md
+
