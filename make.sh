10ca0761c7c1f6254c2e5eb1d6b5007ff74c5b29
jhanrahan <jhanrahan@signalsciences.com>
2020-04-30 15:10:00 -0700

    test with root permissions

--- make.sh
+++ make.sh
@@ -13,7 +13,7 @@ set -ex
 find . -name "goroot" -type d | xargs rm -rf
 mkdir goroot
 docker build -f Dockerfile.git -t golang-git:1.10.6-alpine3.8 .
-docker run --user 1015:1015 -v ${PWD}/goroot:/go/ --rm golang-git:1.10.6-alpine3.8 /bin/sh -c 'go get github.com/signalsciences/tlstext && go get github.com/tinylib/msgp && go get github.com/alecthomas/gometalinter'
+docker run  -v ${PWD}/goroot:/go/ --rm golang-git:1.10.6-alpine3.8 /bin/sh -c ' go get github.com/tinylib/msgp && go get github.com/alecthomas/gometalinter && go get github.com/signalsciences/tlstext'
 ./scripts/build-docker.sh
 
 # run module tests
