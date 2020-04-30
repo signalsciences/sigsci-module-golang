bffbe7c13eaae0d00c62f5e13c0d2cf319977654
jhanrahan <jhanrahan@signalsciences.com>
2020-04-30 15:51:53 -0700

    get info

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,5 +2,10 @@
 
 docker build -t foo  .
 
+echo "***********************************"
+echo `whoami`
+ls -lah goroot
+echo "***********************************"
+
 rm -rf goroot
 docker run -v ${PWD}:/go/src/github.com/signalsciences/sigsci-module-golang --rm foo ./scripts/build.sh
