a1c7d532475a3c1e8962b546a49d7dfb975be4ce
Brian Rectanus <brectanus@signalsciences.com>
2020-06-10 14:13:50 -0400

    build: Cleanup build and test scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,6 @@
 
 set -ex
 
-(cd ./scripts/test-golang110 && ./test.sh)
-(cd ./scripts/test-golang111 && ./test.sh)
+#(cd ./scripts/test-golang110 && ./test.sh)
+#(cd ./scripts/test-golang111 && ./test.sh)
+(cd ./scripts/test-golang114 && ./test.sh)
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package sigsci
 
-const version = "1.7.1"
+const version = "1.8.0"
